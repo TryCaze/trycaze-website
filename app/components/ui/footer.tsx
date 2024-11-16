8fc1493a58110eedc16bd3589d6eb5961febd471
TryCaze <trycaze@gmail.com>
2024-11-16 18:07:43 +0100

    footer updated

--- app/components/ui/footer.tsx
+++ app/components/ui/footer.tsx
@@ -50,87 +50,28 @@ export function FloatingDockDemo() {
   ];
   return (
     <>
-    <section className="py-10 bg-gray-900 sm:pt-16 lg:pt-24">
+    <section className="py-10 bg-slate-900 sm:pt-16 lg:pt-24">
     <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div className="grid grid-cols-2 gap-x-5 gap-y-12 md:grid-cols-4 md:gap-x-12">
-            <div>
-                <p className="text-base text-gray-500">Company</p>
-
-                <ul className="mt-8 space-y-4">
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> About </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Features </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Works </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Career </a>
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <p className="text-base text-gray-500">Help</p>
-
-                <ul className="mt-8 space-y-4">
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Customer Support </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Delivery Details </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Terms & Conditions </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Privacy Policy </a>
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <p className="text-base text-gray-500">Resources</p>
-
-                <ul className="mt-8 space-y-4">
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Free eBooks </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Development Tutorial </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> How to - Blog </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> YouTube Playlist </a>
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <p className="text-base text-gray-500">Extra Links</p>
-
-                <ul className="mt-8 space-y-4">
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Customer Support </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Delivery Details </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Terms & Conditions </a>
-                    </li>
-                    <li>
-                        <a href="#" title="" className="text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Privacy Policy </a>
-                    </li>
-                </ul>
-            </div>
+        <div>
+            <section>
+                <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                    <div className="max-w-2xl mx-auto text-center">
+                        <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl lg:leading-tight">Real humans are here to help you building your brand</h2>
+                        <p className="mt-6 text-lg text-white">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+                        <a href="#" title="" className="inline-flex items-center justify-center px-6 py-4 mt-12 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:bg-blue-700" role="button">
+                        Contact our support
+                        </a>
+                    </div>
+                </div>
+                    <div className="container mx-auto 2xl:px-12">
+                        <div className="flex justify-center items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="82" height="40" fill="none" viewBox="0 0 82 40"><path fill="#FFD43D" d="M73.365 19.71c0 2.904-2.241 5.31-5.27 5.31-3.03 0-5.228-2.406-5.228-5.31 0-2.905 2.199-5.312 5.228-5.312s5.27 2.407 5.27 5.311Z"></path><path fill="#FF0C81" d="M48.764 19.544c0 2.946-2.323 5.145-5.27 5.145-2.904 0-5.227-2.2-5.227-5.145 0-2.947 2.323-5.104 5.228-5.104 2.946 0 5.27 2.158 5.27 5.104Z"></path><path fill="#11EEFC" d="M20.074 25.02c3.029 0 5.27-2.406 5.27-5.31 0-2.905-2.241-5.312-5.27-5.312-3.03 0-5.228 2.407-5.228 5.311 0 2.905 2.199 5.312 5.228 5.312Z"></path><path fill="#171A26" d="M68.095 30.54c-6.307 0-11.12-4.897-11.12-10.872 0-5.934 4.855-10.83 11.12-10.83 6.349 0 11.162 4.938 11.162 10.83 0 5.975-4.855 10.871-11.162 10.871Zm0-5.52c3.03 0 5.27-2.406 5.27-5.31 0-2.905-2.24-5.312-5.27-5.312-3.029 0-5.228 2.407-5.228 5.311 0 2.905 2.199 5.312 5.228 5.312ZM43.08 40c-4.813 0-8.506-2.116-10.373-5.934l4.896-2.655c.913 1.784 2.614 3.195 5.394 3.195 3.486 0 5.85-2.448 5.85-6.473v-.374c-1.12 1.411-3.111 2.49-6.016 2.49-5.768 0-10.373-4.481-10.373-10.581 0-5.934 4.813-10.788 11.12-10.788 6.431 0 11.162 4.605 11.162 10.788v8.299C54.74 35.27 49.76 40 43.08 40Zm.415-15.311c2.946 0 5.27-2.2 5.27-5.145 0-2.947-2.324-5.104-5.27-5.104-2.905 0-5.228 2.158-5.228 5.104s2.323 5.145 5.228 5.145ZM20.074 30.54c-6.307 0-11.12-4.897-11.12-10.872 0-5.934 4.854-10.83 11.12-10.83 6.348 0 11.162 4.938 11.162 10.83 0 5.975-4.855 10.871-11.162 10.871Zm0-5.52c3.029 0 5.27-2.406 5.27-5.31 0-2.905-2.241-5.312-5.27-5.312-3.03 0-5.228 2.407-5.228 5.311 0 2.905 2.199 5.312 5.228 5.312ZM0 0h5.892v30H0V0ZM82 6.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"></path></svg>
+                        </div>
+                    </div>
+                </section>
         </div>
 
-        <hr className="mt-16 mb-10 border-gray-800" />
+        <hr className="mt-16 mb-10 border-slate-600" />
 
         <div className="flex flex-wrap items-center justify-between">
 
@@ -143,7 +84,7 @@ export function FloatingDockDemo() {
                 </div>
             </ul>
 
-            <p className="w-full mt-8 text-sm text-center text-gray-100 md:mt-0 md:w-auto md:order-2">© Copyright 2021, All Rights Reserved by Postcraft</p>
+            <p className="w-full mt-8 text-sm text-center text-white md:mt-0 md:w-auto md:order-2">© Copyright 2024, All Rights Reserved by TryCaze</p>
         </div>
     </div>
 </section>
